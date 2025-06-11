6200c83a478dc7a6cc3a980d8d8721c14d832d74
Ronalzo <2458316043@qq.com>
2025-06-11 18:52:58 +0800

    Add files via upload

---
+++ lab2.c
@@ -0,0 +1,22 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void* print_message(void* ptr) {
+    char* message = (char*) ptr;
+    printf("%s\n", message);
+    return NULL;
+}
+
+int main() {
+    pthread_t thread1, thread2;
+    char* msg1 = "Thread 1";
+    char* msg2 = "Thread 2";
+
+    pthread_create(&thread1, NULL, print_message, (void*) msg1);
+    pthread_create(&thread2, NULL, print_message, (void*) msg2);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    return 0;
+}
